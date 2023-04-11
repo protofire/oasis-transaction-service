6b908fa5442671d2d23d240d4d8a67682733a1b1
datradito <datradito@gmail.com>
2023-04-11 16:28:23 -0300

    feat: adding new master copies & proxy factory supported addresses for mainnet

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -387,8 +387,8 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x92343D7E05Caac0198A47013Abd917461322C9C4", 392329, "1.3.0"),
     ],
     EthereumNetwork.OASIS_SAPPHIRE: [
-        ("0x369C13406825D9F85348B11e2Bb2905D5493313C", 203528, "1.3.0+L2"),
-        ("0x92343D7E05Caac0198A47013Abd917461322C9C4", 203529, "1.3.0"),
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 330092, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 330094, "1.3.0"),
     ],
 }
 
@@ -578,7 +578,7 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
         ("0xb9373Bd08E33C384f6d755196819dA72830Ba509", 392282),  # v1.3.0
     ],
     EthereumNetwork.OASIS_SAPPHIRE: [
-        ("0xb9373Bd08E33C384f6d755196819dA72830Ba509", 203528),  # v1.3.0
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 328339),  # v1.3.0
     ],
 }
 
